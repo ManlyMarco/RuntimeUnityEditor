266e917e8eccdf8119f554eed715face911e9fda
ManlyMarco <dummyaccx@gmail.com>
2019-11-11 19:26:09 +0100

    Look for log in game root to support new BepInEx feature

--- RuntimeUnityEditor/Utils/UnityFeatureHelper.cs
+++ RuntimeUnityEditor/Utils/UnityFeatureHelper.cs
@@ -78,6 +78,9 @@ namespace RuntimeUnityEditor.Core.Utils
             // Generated in most versions unless disabled
             if (TryOpen(Path.Combine(Application.dataPath, "output_log.txt"))) return;
 
+            // Redirected by preloader to game root
+            if (TryOpen(Path.Combine(Path.GetDirectoryName(Application.dataPath) ?? "", "output_log.txt"))) return;
+
             // Available since 2018.3
             var prop = typeof(Application).GetProperty("consoleLogPath", BindingFlags.Static | BindingFlags.Public);
             if (prop != null)
