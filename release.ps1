0921a2e76a6fce65759108ae4f295da28b7efd59
ManlyMarco <dummyaccx@gmail.com>
2019-11-30 16:52:59 +0100

    New release setup

---
+++ release.ps1
@@ -0,0 +1,26 @@
+if ($PSScriptRoot -match '.+?\\bin\\?') {
+    $dir = $PSScriptRoot + "\"
+}
+else {
+    $dir = $PSScriptRoot + "\bin\"
+}
+
+$copy = $dir + "\copy\BepInEx" 
+
+$ver = "v" + (Get-ChildItem -Path ($dir + "\BepInEx\") -Filter "*.dll" -Recurse -Force)[0].VersionInfo.FileVersion.ToString()
+
+New-Item -ItemType Directory -Force -Path ($dir + "\out")  
+
+Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
+New-Item -ItemType Directory -Force -Path ($copy + "\plugins")
+Copy-Item -Path ($dir + "\BepInEx\plugins\") -Destination ($copy) -Recurse -Force 
+Remove-Item -Force -Path ($copy + "\plugins\RuntimeUnityEditor\RuntimeUnityEditor.Bepin4.dll") -Recurse
+Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + "RuntimeUnityEditor_BepInEx5_" + $ver + ".zip")
+
+Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
+New-Item -ItemType Directory -Force -Path ($copy + "\plugins")
+Copy-Item -Path ($dir + "\BepInEx\plugins\") -Destination ($copy) -Recurse -Force 
+Remove-Item -Force -Path ($copy + "\plugins\RuntimeUnityEditor\RuntimeUnityEditor.Bepin5.dll") -Recurse
+Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + "RuntimeUnityEditor_BepInEx4_" + $ver + ".zip")
+
+Remove-Item -Force -Path ($dir + "\copy") -Recurse
