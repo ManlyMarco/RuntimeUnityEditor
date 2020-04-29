183e80cdef850d49791e55bb3b419a5842eac1a9
ManlyMarco <dummyaccx@gmail.com>
2020-04-29 11:23:17 +0200

    Fixed wrong order of log file getting

--- RuntimeUnityEditor/Utils/UnityFeatureHelper.cs
+++ RuntimeUnityEditor/Utils/UnityFeatureHelper.cs
@@ -69,11 +69,11 @@ namespace RuntimeUnityEditor.Core.Utils
                 }
             }
 
+            // Redirected by preloader to game root
+            if (TryOpen(Path.Combine(Path.Combine(Application.dataPath, ".."), "output_log.txt"))) return;
+
             // Generated in most versions unless disabled
             if (TryOpen(Path.Combine(Application.dataPath, "output_log.txt"))) return;
-
-            // Redirected by preloader to game root
-            if (TryOpen(Path.Combine(Path.GetDirectoryName(Application.dataPath) ?? "", "output_log.txt"))) return;
 
             // Available since 2018.3
             var prop = typeof(Application).GetProperty("consoleLogPath", BindingFlags.Static | BindingFlags.Public);
