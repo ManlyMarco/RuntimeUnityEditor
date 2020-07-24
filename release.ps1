fd7a3a02d8bd642c4bb11233127213224e410da2
ManlyMarco <dummyaccx@gmail.com>
2020-07-24 03:00:11 +0200

    Fix release script

--- release.ps1
+++ release.ps1
@@ -12,17 +12,15 @@ $ver = "v" + (Get-ChildItem -Path ($dir + "\BepInEx\") -Filter "*.dll" -Recurse 
 New-Item -ItemType Directory -Force -Path ($dir + "\out")  
 
 Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Force -Path ($copy + "\plugins")
+New-Item -ItemType Directory -Force -Path ($copy + "\plugins\RuntimeUnityEditor")
 Copy-Item -Path ($dir + "\BepInEx\plugins\") -Destination ($copy) -Recurse -Force 
 Remove-Item -Force -Path ($copy + "\plugins\RuntimeUnityEditor\RuntimeUnityEditor.Bepin4.dll") -Recurse
 Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + "RuntimeUnityEditor_BepInEx5_" + $ver + ".zip")
 
 Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
 New-Item -ItemType Directory -Force -Path ($copy)
-Copy-Item -Path ($dir + "\BepInEx\plugins\RuntimeUnityEditor\*") -Destination ($copy) -Recurse -Force 
-Remove-Item -Force -Path ($copy + "\LICENSE") -Recurse
-Remove-Item -Force -Path ($copy + "\README.md") -Recurse
-Remove-Item -Force -Path ($copy + "\RuntimeUnityEditor.Bepin5.dll") -Recurse
+Copy-Item -Path ($dir + "\BepInEx\plugins\") -Destination ($copy) -Recurse -Force 
+Remove-Item -Force -Path ($copy + "\plugins\RuntimeUnityEditor\RuntimeUnityEditor.Bepin5.dll") -Recurse
 Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + "RuntimeUnityEditor_BepInEx4_" + $ver + ".zip")
 
 Remove-Item -Force -Path ($dir + "\copy") -Recurse
