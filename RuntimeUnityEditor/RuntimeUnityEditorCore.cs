0322b193b3fa7c0e19cccbc6aaca250166e93d8c
ManlyMarco <dummyaccx@gmail.com>
2021-11-13 11:25:52 +0100

    Version up to 2.6

--- RuntimeUnityEditor/RuntimeUnityEditorCore.cs
+++ RuntimeUnityEditor/RuntimeUnityEditorCore.cs
@@ -13,7 +13,7 @@ namespace RuntimeUnityEditor.Core
 {
     public class RuntimeUnityEditorCore
     {
-        public const string Version = "2.5";
+        public const string Version = "2.6";
         public const string GUID = "RuntimeUnityEditor";
 
         public Inspector.Inspector Inspector { get; }
