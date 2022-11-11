142118265ecf4a26241054194f39487a6a74892f
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2022-11-11 03:49:58 +0100

    Remove bepin4 version from release script

--- release.ps1
+++ release.ps1
@@ -14,13 +14,6 @@ New-Item -ItemType Directory -Force -Path ($dir + "\out")
 Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
 New-Item -ItemType Directory -Force -Path ($copy + "\plugins\RuntimeUnityEditor")
 Copy-Item -Path ($dir + "\BepInEx\plugins\") -Destination ($copy) -Recurse -Force 
-Remove-Item -Force -Path ($copy + "\plugins\RuntimeUnityEditor\RuntimeUnityEditor.Bepin4.dll") -Recurse
 Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + "RuntimeUnityEditor_BepInEx5_" + $ver + ".zip")
-
-Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Force -Path ($copy)
-Copy-Item -Path ($dir + "\BepInEx\plugins\") -Destination ($copy) -Recurse -Force 
-Remove-Item -Force -Path ($copy + "\plugins\RuntimeUnityEditor\RuntimeUnityEditor.Bepin5.dll") -Recurse
-Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + "RuntimeUnityEditor_BepInEx4_" + $ver + ".zip")
 
 Remove-Item -Force -Path ($dir + "\copy") -Recurse
