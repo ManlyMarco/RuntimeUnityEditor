4a0c31db75c10e53f84e378619827a31f38312d4
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2024-04-25 19:21:47 +0200

    Version up to 5.3.1

--- RuntimeUnityEditor/RuntimeUnityEditorCore.cs
+++ RuntimeUnityEditor/RuntimeUnityEditorCore.cs
@@ -24,7 +24,7 @@ namespace RuntimeUnityEditor.Core
         /// Beware that this is a const and it will be burned as a string into your assembly at build time.
         /// To see the version that is currently installed use <see cref="InstalledVersion"/>.
         /// </summary>
-        public const string Version = "5.3.0";
+        public const string Version = "5.3.1";
 
         /// <summary>
         /// Get the currently installed version at runtime.
