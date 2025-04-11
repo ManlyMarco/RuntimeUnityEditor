4fd318b01f76f0ccf8a1cf462e57fba4ae625ace
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-04-11 13:22:47 +0200

    [UMM] Clean up the UMM plugin class; Fix some minor issues

--- RuntimeUnityEditor.UMM/RuntimeUnityEditorUMM.cs
+++ RuntimeUnityEditor.UMM/RuntimeUnityEditorUMM.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using UnityModManagerNet;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable IDE0051 // Remove unused private members
 
 namespace RuntimeUnityEditor.UMM
 {
@@ -13,15 +14,20 @@ namespace RuntimeUnityEditor.UMM
     /// When referencing RuntimeUnityEditor from other code it's recommended to not reference this assembly and instead reference RuntimeUnityEditorCore directly.
     /// You can see if RuntimeUnityEditor has finished loading with <code>RuntimeUnityEditorCore.IsInitialized()</code>.
     /// </summary>
-    [Obsolete("It's recommended to reference RuntimeUnityEditorCore directly")] // TODO: Since core dll is ILMerged and internalized this is impossible to do
+    [Obsolete("It's recommended to reference RuntimeUnityEditorCore directly")]
     public static class RuntimeUnityEditorUMM
     {
-        public static bool Enabled { get; private set; }
+        public static bool Enabled
+        {
+            get => _editorMonoBehaviour?.enabled ?? false;
+            private set => _editorMonoBehaviour.enabled = value;
+        }
         public static UnityModManager.ModEntry Instance { get; private set; }
         public static RuntimeUnityEditorCore CoreInstance { get; private set; }
-        public static RuntimeUnityEditorSettings Settings;
-        private static GameObject runtimeUnityEditorGO;
-        private static MonoBehaviour RuntimeUnityEditor;
+        public static RuntimeUnityEditorSettings Settings { get; private set; }
+
+        private static GameObject _editorGameObject;
+        private static MonoBehaviour _editorMonoBehaviour;
 
         public static bool Load(UnityModManager.ModEntry modEntry)
         {
@@ -33,11 +39,11 @@ namespace RuntimeUnityEditor.UMM
 
             Instance = modEntry;
 
-            Settings = RuntimeUnityEditorSettings.Load<RuntimeUnityEditorSettings>(Instance);
+            Settings = UnityModManager.ModSettings.Load<RuntimeUnityEditorSettings>(Instance);
             Settings.Load(Instance);
 
-            runtimeUnityEditorGO = new GameObject("RuntimeUnityEditor", typeof(RuntimeUnityEditorBehaviour));
-            UnityEngine.Object.DontDestroyOnLoad(runtimeUnityEditorGO);
+            _editorGameObject = new GameObject("RuntimeUnityEditor", typeof(RuntimeUnityEditorBehaviour));
+            UnityEngine.Object.DontDestroyOnLoad(_editorGameObject);
 
             Instance.OnGUI = OnGUI;
             Instance.OnSaveGUI = Settings.Save;
@@ -52,37 +58,27 @@ namespace RuntimeUnityEditor.UMM
 
         private class RuntimeUnityEditorBehaviour : MonoBehaviour
         {
-            void Start()
+            private void Start()
             {
-                RuntimeUnityEditor = this;
+                _editorMonoBehaviour = this;
                 CoreInstance = new RuntimeUnityEditorCore(new UMMInitSettings(Instance, Settings));
             }
-            private void Update()
-            {
-                CoreInstance.Update();
-            }
 
-            private void LateUpdate()
-            {
-                CoreInstance.LateUpdate();
-            }
+            private void Update() => CoreInstance.Update();
 
-            private void OnGUI()
-            {
-                CoreInstance.OnGUI();
-            }
+            private void LateUpdate() => CoreInstance.LateUpdate();
+
+            private void OnGUI() => CoreInstance.OnGUI();
         }
 
         private sealed class UMMInitSettings : InitSettings
         {
-            private readonly UnityModManager.ModEntry _instance;
-            private RuntimeUnityEditorSettings _settings;
+            private readonly RuntimeUnityEditorSettings _settings;
 
             public UMMInitSettings(UnityModManager.ModEntry instance, RuntimeUnityEditorSettings settings)
             {
-                _instance = instance;
                 _settings = settings;
-                LoggerWrapper = new LoggerUMM(_instance.Logger);
+                LoggerWrapper = new LoggerUMM(instance.Logger);
             }
 
             protected override Action<T> RegisterSetting<T>(string category, string name, T defaultValue, string description, Action<T> onValueUpdated)
@@ -94,11 +90,11 @@ namespace RuntimeUnityEditor.UMM
                 }
                 setting.OnChanged = onValueUpdated;
                 onValueUpdated(setting.Value);
-     
+
                 return (newValue) => { setting.Value = newValue; onValueUpdated(newValue); };
             }
 
-            public override MonoBehaviour PluginMonoBehaviour => RuntimeUnityEditor;
+            public override MonoBehaviour PluginMonoBehaviour => _editorMonoBehaviour;
             public override ILoggerWrapper LoggerWrapper { get; }
             public override string ConfigPath => Instance.Path;
         }
@@ -116,24 +112,32 @@ namespace RuntimeUnityEditor.UMM
             {
                 switch (logLevel)
                 {
+                    case LogLevel.All:
                     case LogLevel.None:
                     case LogLevel.Fatal:
-                        if (content is Exception) _logger.LogException(content as Exception);
-                        else _logger.Critical(content as string); 
+                        if (content is Exception e1) _logger.LogException(e1);
+                        else _logger.Critical(content as string);
                         break;
+
                     case LogLevel.Error:
-                        if (content is Exception) _logger.LogException(content as Exception);
+                        if (content is Exception e2) _logger.LogException(e2);
                         else _logger.Error(content as string);
                         break;
+
                     case LogLevel.Warning:
-                        if (content is Exception) _logger.LogException(content as Exception);
+                        if (content is Exception e3) _logger.LogException(e3);
                         else _logger.Warning(content as string);
                         break;
+
                     case LogLevel.Message:
                     case LogLevel.Info:
                     case LogLevel.Debug:
-                        if (content is Exception) _logger.LogException(content as Exception);
+                        if (content is Exception e4) _logger.LogException(e4);
                         else _logger.Log(content as string);
+                        break;
+
+                    default:
+                        _logger.Warning($"Unknown LogLevel [{logLevel}] for content: {content}");
                         break;
                 }
             }
