90eafaacc4961012d51e8d0d906b904c4c0129a5
Kein <kein-of@yandex.ru>
2025-10-09 23:01:00 +0300

    Don't bother with method restoration, just use raw GLOpt (#115)

--- RuntimeUnityEditor.Core/Utils/Abstractions/GUILayoutShim.cs
+++ RuntimeUnityEditor.Core/Utils/Abstractions/GUILayoutShim.cs
@@ -7,21 +7,38 @@ namespace RuntimeUnityEditor.Core
         public static GUILayoutOption MaxWidth(float width)
         {
 #if IL2CPP
-            var entity = GUILayout.ExpandHeight(true);
-            entity.type = GUILayoutOption.Type.maxWidth;
-            entity.value = width;
+            var entity = new UnityEngine.GUILayoutOption(GUILayoutOption.Type.maxWidth, width);
             return entity;
 #else
             return GUILayout.MaxWidth(width);
 #endif
         }
 
+         public static GUILayoutOption ExpandWidth(bool expand)
+        {
+#if IL2CPP
+            var entity = new UnityEngine.GUILayoutOption(GUILayoutOption.Type.stretchWidth, expand);
+            return entity;
+#else
+            return GUILayout.ExpandWidth(expand);
+#endif
+        }
+
+        public static GUILayoutOption ExpandHeight(bool expand)
+        {
+#if IL2CPP
+            var entity = new UnityEngine.GUILayoutOption(GUILayoutOption.Type.stretchHeight, expand);
+            return entity;
+#else
+            return GUILayout.ExpandHeight(expand);
+#endif
+        }
+
+
         public static GUILayoutOption MinWidth(float width)
         {
 #if IL2CPP
-            var entity = GUILayout.ExpandHeight(true);
-            entity.type = GUILayoutOption.Type.minWidth;
-            entity.value = width;
+            var entity = new UnityEngine.GUILayoutOption(GUILayoutOption.Type.minWidth, width);
             return entity;
 #else
             return GUILayout.MinWidth(width);
